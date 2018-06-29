9b43311b7c3c8d3559975096623df2a76d5e8bf1
吴才龙 <hengxingyema@163.com>
2018-06-29 10:15:53 +0800

    e
    ee

--- server.js
+++ server.js
@@ -3,9 +3,9 @@
 var AV = require('leanengine');
 
 AV.init({
-  appId: process.env.LEANCLOUD_APP_ID,
-  appKey: process.env.LEANCLOUD_APP_KEY,
-  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
+  appId: "CWvhidlyThey3vYpeIXRtJ2S-gzGzoHsz",
+  appKey: "QkqgETVwPf4L1sqT8bKbRUu3",
+  masterKey: "PARCtfnEmyXLcBG5XOV3QgEi"
 });
 
 // 如果不希望使用 masterKey 权限，可以将下面一行删除
